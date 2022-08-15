19a01b43b6e406e195dc9617039a3d30639de14c
daris <daris@DESKTOP-3F1ME0J>
2022-08-15 19:11:30 +0900

    Add project files.

---
+++ Area of a Circle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14159;
+            double rad = Convert.ToDouble(Console.ReadLine());
+            double area = pi * rad * rad;
+            Console.WriteLine("Area of the circle is " + area + ".");
+
+
+        }
+    }
+}
